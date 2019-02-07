eda7d181daec587941e875cfc3b54e9155cf4b83
Elian Fudulu <40577574+elielbeck@users.noreply.github.com>
2019-02-07 22:36:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('igU4iI8mVMqo6xtQc1RGxsmDPkqo55Vd'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
